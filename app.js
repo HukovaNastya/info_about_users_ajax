8a39b452b52ff48f9feae59b6cd1d3cfc9d08e05
HukovaNastya <62759919+HukovaNastya@users.noreply.github.com>
2021-05-25 17:22:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const usersContainer = document.querySelector('.content');
 const usersCount = 10;
+const postsCount = 100
 const tabsBody = document.querySelector('.tabs__body');
 let users;
-const tabsList =  document.querySelectorAll('.tabs__block');
-// console.log(tabsList)
-
-
-
 
 async function request(route, options = { method: "GET" }) {
   const API_URL = "https://jsonplaceholder.typicode.com";
@@ -34,6 +30,47 @@ async function getAllUsers() {
   }
 }
 
+//Posts
+async function fetchUsersPosts (userId){
+  try{
+    const response = await request(`posts/${userId}`);
+    // console.log(response);
+    return response;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+fetchUsersPosts(1);
+
+async function getAllUsersPosts () {
+  try {
+    const arrayOfPromises = Array.from(new Array(postsCount)).map((post, index) => fetchUsersPosts(index + 1));
+    const arrayOfPostsData = await Promise.all(arrayOfPromises);
+    console.log(arrayOfPostsData);
+    return arrayOfPostsData;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// getAllUsersPosts();
+// async function getAllPostsOfUsers(id) {
+//   try {
+//     const user = await fetchUsers(id);
+//     const postsPromises = user.id.map((id) => fetchUsersPosts (userId));
+//     const posts = await Promise.all(postsPromises);
+//     return posts;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// getAllPostsOfUsers();
+
+// TODO создать функцию получения постов по id пользователя GET https://jsonplaceholder.typicode.com/posts?userId=1
+
 function renderTabs(users) {
   const wrapper = createTabsWrapper()
   users.forEach(user => wrapper.insertAdjacentHTML('beforeend', createTabItem(user.id, user.name)))
@@ -58,79 +95,125 @@ function createTabItem(id, name) {
 function setTabsEvents() {
   const items = document.querySelectorAll('.tabs__item')
   items.forEach(item => item.addEventListener('click', tabItemClickHandler));
-
 }
 
 function renderTabsContent(users) {
-  // console.log(users)
   const tabsBlock = createTabsContentBody();
-  users.forEach(user => tabsBlock.insertAdjacentHTML('beforeend', createTabBlock(user.id, user.username, user.email, user.address)));
+  users.forEach(user => tabsBlock.insertAdjacentHTML('beforeend', createTabBlock(user)));
   tabsBody.appendChild(tabsBlock);
 }
 
 function createTabsContentBody() {
   const tabsBlock = document.createElement('div');
-  tabsBlock.classList.add('tabs__block');
-
+  tabsBlock.classList.add('tabs__content-list');
   return tabsBlock;
 }
 
-function createTabBlock(id, username, email, address) {
-  const tabStr = `
-    <div id="tab_${id}" data-user-id="${id}" class="tabs__list">
+function createTabBlock({id, username, email, address}) {
+  return `
+    <div id="tab_${id}" data-user-id="${id}" class="tabs__block">
        <p> username: ${username} (email:  ${email})</p>
        <p> ${addressToString(address)} </p>
        <p> ${geoToString(address.geo)} </p>
     </div>
   `;
-  // console.log(tabStr)
-  return tabStr;
 }
 
-function addressToString(address) {
-  const addressSring = `${address.zipcode} - ${address.city}, ${address.street} ${address.suite}`;
-  return addressSring;
+function addressToString({ zipcode, city, street, suite } = {}) {
+  return `${zipcode} - ${city}, ${street} ${suite}`;
 }
 
-function geoToString(geo) {
-  const str = `X: ${geo.lat} Y: ${geo.lng}`;
-  return str;
+function geoToString({lat = '', lng = ''} = {}) {
+  return `X: ${lat} Y: ${lng}`;
 }
 
 function tabItemClickHandler(evt) {
   const target = evt.currentTarget.getAttribute('href');
+  // const userId = evt.currentTarget.getAttribute('data-user-id')
+  // console.log(userId)
+  // if(users.id === posts.userId){
+  //   renderTabsContentPosts(posts)
+  // }
 
-  console.log(target);
+  deactivateActiveTabContent()
+  setActiveTabContent(target)
+}
 
-  let currentTab = document.querySelector(target);
-  console.log(currentTab)
-  // let tabsBtn = document.querySelector('.tabs__block-active');
+// function getUserId (evt){
+//   const userId = evt.currentTarget.getAttribute('data-user-id')
+//   console.log(userId)
+// }
 
 
+function setActiveTabContent(target) {
+  const currentTab = document.querySelector(target);
+  console.log(currentTab);
+  const id = currentTab.getAttribute('data-user-id');
+  console.log(id);
 
+  // const filterPosts = posts.filter(post => post.userId === id);
+  // console.log(filterPosts )
 
-  if(currentTab){
-    tabsList.forEach(function(item){
-      item.classList.remove('tabs__block-active')
-
-     })
-
-  }
-
+  // TODO Получить id пользователя из data-user-id
+  if (!currentTab) return
   currentTab.classList.add('tabs__block-active');
+  // currentTab += getUserPostsHandler();
+  // // getUserPostsHandler(id)
 
+}
+
+
+// async function filterPosts (users, posts){
+//   users = await getAllUsers();
+//   posts = await getAllUsersPosts();
+//   const getFilterPosts = posts.filter(post => post.userId === users.id);
+//   console.log(getFilterPosts)
+// }
+// filterPosts()
+// TODO getUserPostsHandler
+function getUserPostsHandler({title, body}){
+  return `
+  <div  class="tabs__block-active">
+     <p> title: ${title} </br> body: ${body})</p>
+  </div>
+`;
+}
+
+
+function renderTabsContentPosts(posts) {
+  const tabsBlock = createTabsContentBody();
+
+  // console.log(posts)
+  posts.forEach(post => tabsBlock.insertAdjacentHTML('beforeEnd', getUserPostsHandler(post)));
+  tabsBody.appendChild(tabsBlock);
+  console.log(tabsBody)
+}
+
+
+
+
+function deactivateActiveTabContent() {
+  const currentActiveTab = document.querySelector('.tabs__block-active')
+  if (currentActiveTab) {
+    currentActiveTab.classList.remove('tabs__block-active')
+  }
+}
+
+function setFirstActiveTab(users) {
+  const [user] = users
+  const firstTabContentId = `#tab_${user.id}`
+  setActiveTabContent(firstTabContentId)
 }
 
 async function initApp() {
   try {
-    users = await getAllUsers()
+    users = await getAllUsers();
+    posts = await getAllUsersPosts();
     renderTabs(users)
     renderTabsContent(users)
+    renderTabsContentPosts(posts)
     setTabsEvents()
-
-
-    // TODO сделать первый таб и его контент активным.
-    // TODO Получить данные про посты пользователя GET https://jsonplaceholder.typicode.com/posts?userId=1
+    setFirstActiveTab(users)
   } catch (err) {
     console.error(err)
   }
